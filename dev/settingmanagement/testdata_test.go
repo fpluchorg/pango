f4075dad4b402ebae3af267fa202747037e20745
josephbacha <joseph.elbacha@gmail.com>
2024-05-29 15:45:34 +0200

    Enhancement unit testing

--- dev/settingmanagement/testdata_test.go
+++ dev/settingmanagement/testdata_test.go
@@ -12,12 +12,20 @@ type testCase struct {
 
 func getTests() []testCase {
 	trueVal := true
+	intVal := 1
 	return []testCase{
 		{"test device and panos", version.Number{10, 0, 0, ""}, Config{
 			EnableLogHighDpLoad:          &trueVal,
 			EnableHighSpeedLogForwarding: &trueVal,
 			SupportUtf8ForLogOutput:      &trueVal,
 			TrafficStopOnLogdbFull:       &trueVal,
+			ThreatVaultAccess:            &trueVal,
+			HostnameTypeInSyslog:         "hostname",
+			FailedAttempts:               &intVal,
+			LockoutTime:                  &intVal,
+			MaxSessionTime:               &intVal,
+			MaxSessionCount:              &intVal,
+			IdleTimeout:                  &intVal,
 		}},
 	}
 }
